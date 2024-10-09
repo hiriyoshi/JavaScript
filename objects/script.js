a3560d77d45ce8b5c1cff12bfdf758e6c4105276
hiriyoshi <morita.mcp@gmail.com>
2024-10-10 04:49:56 +0800

    dodge and flee functions complete

--- objects/script.js
+++ objects/script.js
@@ -3,13 +3,14 @@ const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
 const btn4 = document.querySelector('.btn4');
 const text = document.querySelector('.text');
+const editable = document.querySelector('.editable');
 const healthStat = document.querySelector('.health');
 const goldStat = document.querySelector('.gold');
 const weaponStat = document.querySelector('.weapon');
 const skillsStat = document.querySelector('.skills');
 const currentWeapon = ["Wooden Sword"];
 const currentSkill = ["Confuse"];
-let gold = 100;
+let gold = 500;
 let health = 100;
 let currentHealth = 10;
 let showhealth = `${currentHealth}/${health}`;
@@ -18,9 +19,40 @@ let intervalId;
 let healing;
 let heals = false;
 
-const home = () => update(loc[0])
+const home = () => update(loc[0]);
 const guild = () => update(loc[1]);
-const wild = () => update(loc[2]);
+const wild = () => {
+  update(loc[2]);
+  const div = document.createElement('div');
+  div.className = "monsterstat";
+  div.style.display = "flex";
+  div.style.alignItems = "center";
+  div.style.justifyContent = "center";
+  editable.appendChild(div);
+
+  const monsterName = document.createElement('p');
+  monsterName.innerHTML = `Enemy Name: ${wildEnemyRandom()}`;
+  monsterName.style.width = "200px";
+  monsterName.style.height = "30px";
+  monsterName.style.display = "flex";
+  monsterName.style.alignItems = "center";
+  monsterName.style.fontWeight = "bold";
+  monsterName.style.color = "white";
+  monsterName.style.textShadow = "3px 3px 3px red, 1px 3px 3px red, 2px -2px 3px red, -2px -2px 3px red";
+  div.appendChild(monsterName);
+
+  const monsterHealth = document.createElement('p');
+  monsterHealth.innerHTML = `Enemy Health: ${wildEnemy.find(wildEnemyRandom).health} / ${wildEnemy.find(wildEnemyRandom).health}`;
+  monsterHealth.style.width = "200px";
+  monsterHealth.style.height = "30px";
+  monsterHealth.style.display = "flex";
+  monsterHealth.style.alignItems = "center";
+  monsterHealth.style.fontWeight = "bold";
+  monsterHealth.style.color = "white";
+  monsterHealth.style.textShadow = "3px 3px 3px red, 1px 3px 3px red, 2px -2px 3px red, -2px -2px 3px red";
+  div.appendChild(monsterHealth);
+
+}
 const cave = () => update(loc[3]);
 const store = () => update(loc[4]);
 const ranks = () => update(loc[5]);
@@ -124,7 +156,7 @@ const restart = () =>{
   currentSkill.querySelector('span').innerText = currentSkill[0];
   home(); 
 }
-
+//add some tips while healing in the guild!
 const guildHeal = () =>{
   heal();
   guild();
@@ -134,15 +166,24 @@ const heal = () =>{
   if(currentHealth<health){
     healing = setInterval(()=>{
       if(currentHealth<health && btnUpdate.name === "Guild"){
+        healthStat.querySelector('span').style.color = "white";   
+        setTimeout(()=>{
+          healthStat.querySelector('span').style.color = "white";
+          healthStat.querySelector('span').style.animation = 'color 0.3s ease';
+        },250)
+        setTimeout(()=>{
+          healthStat.querySelector('span').style.color = "red";
+          healthStat.querySelector('span').style.animation = 'color 0.3s ease';
+        },500)
         currentHealth +=2;
-        showhealth = `${currentHealth}/${health}`;
+        showhealth = `${currentHealth}/${health}`;      
         healthStat.querySelector('span').innerText = showhealth;
         console.log("Healing");
       }
       else{
         clearInterval(healing);
       }
-    },1000);
+    },500);
   }
 }
 
@@ -160,7 +201,8 @@ const buyHealth = () =>{
     text.innerText = " You do not have sufficient gold to buy Max health"
   }
 }
-
+//make the weapons to be button and reselect their weapon to change it.
+// make effect to look that it is being used
 const weapons = [
   {
     name: "Wooden Sword",
@@ -193,10 +235,71 @@ const weapons = [
     gold: 300
   },
 ];
-const bronzeSword = () =>{}
-const ironSword = () =>{}
-const silverSword = () =>{}
-const goldSword = () =>{}
+const bronzeSword = () =>{
+  if(gold>=50 && !currentWeapon.some(char => char === 'Bronze Sword')){
+    gold -=50;
+    currentWeapon.pop();
+    currentWeapon.push("Bronze Sword");
+    weaponStat.querySelector('span').innerText = currentWeapon;
+    text.innerText = "You have purchased a Bronze Sword. Glory to Adventure!"
+    goldStat.querySelector('span').innerHTML = gold;
+  }
+  else if(currentWeapon.some(char => char === 'Bronze Sword')){
+    text.innerText = " You already have a Bronze Sword.";
+  }
+  else{
+    text.innerText = " You do not have sufficient gold to buy a Bronze Sword.";
+  }
+}
+// still a bug about buying high to low
+const ironSword = () =>{
+  if(gold>=100 && !currentWeapon.some(char => char === 'Iron Sword')){
+    gold -=100;
+    currentWeapon.pop();
+    currentWeapon.push("Iron Sword");
+    weaponStat.querySelector('span').innerText = currentWeapon;
+    text.innerText = "You have purchased a Iron Sword. Glory to Adventure!"
+    goldStat.querySelector('span').innerHTML = gold;
+  }
+  else if(currentWeapon.some(char => char === 'Iron Sword')){
+    text.innerText = " You already have a Iron Sword.";
+  }
+  else{
+    text.innerText = " You do not have sufficient gold to buy a Iron Sword.";
+  }
+}
+const silverSword = () =>{
+  if(gold>=150 && !currentWeapon.some(char => char === 'Silver Sword')){
+    gold -=150;
+    currentWeapon.pop();
+    currentWeapon.push("Silver Sword");
+    weaponStat.querySelector('span').innerText = currentWeapon;
+    text.innerText = "You have purchased a Silver Sword. Glory to Adventure!"
+    goldStat.querySelector('span').innerHTML = gold;
+  }
+  else if(currentWeapon.some(char => char === 'Silver Sword')){
+    text.innerText = " You already have a Silver Sword.";
+  }
+  else{
+    text.innerText = " You do not have sufficient gold to buy Silver Sword.";
+  }
+}
+const goldSword = () =>{
+  if(gold>=300 && !currentWeapon.some(char => char === 'Gold Sword')){
+    gold -=300;
+    currentWeapon.pop();
+    currentWeapon.push("Gold Sword");
+    weaponStat.querySelector('span').innerText = currentWeapon;
+    text.innerText = "You have purchased a Gold Sword. Glory to Adventure!"
+    goldStat.querySelector('span').innerHTML = gold;
+  }
+  else if(currentWeapon.some(char => char === 'Gold Sword')){
+    text.innerText = " You already have a Gold Sword.";
+  }
+  else{
+    text.innerText = " You do not have sufficient gold to buy a Gold Sword.";
+  }
+}
 const skills = [
   {
     name: "confuse",
@@ -231,27 +334,70 @@ const skills = [
 ];
 const poison = () =>{
   if(gold>=50 && !currentSkill.some(char => char === 'Poison')){
-    currentSkill.push('Poison');
-    currentSkill.querySelector('span').innerText = 'Poison';
-    goldStat -= 50;
-    goldStat.querySelector('span').innerText = goldStat;
-    currentSkill.querySelector('span').innerText = currentSkill;
+    gold -=50;
+    goldStat.querySelector('span').innerHTML = gold;
+    currentSkill.push("Poison");
+    skillsStat.querySelector('span').innerText = currentSkill;
+    text.innerText = "You have purchased a skill 'Poison'. Glory to Adventure!"
+
+  }
+  else if(currentSkill.some(char => char === 'Poison')){
+    text.innerText = " You already have a skill 'Poison'.";
   }
   else{
-    text.innerText = "You do not have sufficient gold to buy this."
-  }  
-}
-const blind = () =>{
-  if(!currentSkill.some(char => char === 'Blind')){
-    currentSkill.push('Blind');
-  }
-}
-const bleed = () =>{
-  if(!currentSkill.some(char => char === 'Bleed')){
-    currentSkill.push('Bleed');
+    text.innerText = " You do not have sufficient gold to buy a skill 'Poison'.";
   }
 }
 
+const blind = () =>{
+  if(gold>=100 && !currentSkill.some(char => char === 'Blind')){
+    gold -=100;
+    goldStat.querySelector('span').innerHTML = gold;
+    currentSkill.push("Blind");
+    skillsStat.querySelector('span').innerText = currentSkill;
+    text.innerText = "You have purchased a skill 'Blind'. Glory to Adventure!"
+
+  }
+  else if(currentSkill.some(char => char === 'Blind')){
+    text.innerText = " You already have a skill 'Blind'.";
+  }
+  else{
+    text.innerText = " You do not have sufficient gold to buy a skill 'Blind'.";
+  }
+}
+
+const bleed = () =>{
+  if(gold>=200 && !currentSkill.some(char => char === 'Bleed')){
+    gold -=200;
+    goldStat.querySelector('span').innerHTML = gold;
+    currentSkill.push("Bleed");
+    skillsStat.querySelector('span').innerText = currentSkill;
+    text.innerText = "You have purchased a skill 'Bleed'. Glory to Adventure!"
+
+  }
+  else if(currentSkill.some(char => char === 'Bleed')){
+    text.innerText = " You already have a skill 'Bleed'.";
+  }
+  else{
+    text.innerText = " You do not have sufficient gold to buy a skill 'Bleed'.";
+  }
+}
+const bash = () =>{
+  if(gold>=300 && !currentSkill.some(char => char === 'Bash')){
+    gold -=300;
+    goldStat.querySelector('span').innerHTML = gold;
+    currentSkill.push("Bash");
+    skillsStat.querySelector('span').innerText = currentSkill;
+    text.innerText = "You have purchased a skill 'Bash'. Glory to Adventure!"
+
+  }
+  else if(currentSkill.some(char => char === 'Bash')){
+    text.innerText = " You already have a skill 'Bash'.";
+  }
+  else{
+    text.innerText = " You do not have sufficient gold to buy a skill 'Poison'.";
+  }
+}
 const wildEnemy = [
   {
     name: "Bandit",
@@ -299,6 +445,49 @@ const caveEnemy = [
   }
 ];
 
+const wildEnemyRandom = () =>{
+  
+  return wildEnemy[Math.floor(Math.random()*3)].name;
+}
+const caveEnemyRandom = () =>{
+  
+  return caveEnemy[Math.floor(Math.random()*3)].name;
+}
+
+const liveFight = () =>{
+  //name
+  //health
+  const div = document.createElement('div');
+  div.className = "monsterstat";
+  div.appendChild(editable);
+}  
+
+const dodge = () =>{
+  const fleePercentage = Math.floor(Math.random()*100)+1;
+  if(fleePercentage<=80){
+    text.innerText = "You have Successfully dodge the attack."
+  }
+  else{
+    text.innerText = "You have Failed to dodge the attack."
+  }
+}
+
+const flee = () =>{
+  const fleePercentage = Math.floor(Math.random()*100)+1;
+  if(fleePercentage<=80){
+    const remove = document.querySelector('.monsterstat');
+    if (remove) {
+      remove.remove();
+    }
+    guild();
+  }
+  else{
+    text.innerText = "You have Failed to Flee."
+  }
+}
+const attack = () =>{}
+const activeSkill = () =>{}
+
 const loc =[
     {
       name: "Home",
@@ -314,15 +503,15 @@ const loc =[
     },
     {
       name: "Wild",
-      "button text": ["Skills","Dodge","Observe","Flee"],
-      "button func" :[],
-      text: `You have encounter an ${wildEnemy[Math.floor(Math.random()*3)].name} in the Wild.`
+      "button text": ["Attack","Skills","Dodge","Flee"],
+      "button func" :[,,dodge,flee],
+      text: `You have encounter an ${wildEnemyRandom()} in the Wild.`
     },
     {
       name: "Cave",
       "button text": ["Attack","Dodge","Skills","Flee"],
-      "button func" :[],
-      text: `You have encounter an ${caveEnemy[Math.floor(Math.random()*3)].name} in the Cave.`
+      "button func" :[,,dodge,flee],
+      text: `You have encounter an ${caveEnemyRandom()} in the Cave.`
     },
     {
       name: "Store",
@@ -345,7 +534,7 @@ const loc =[
     {
       name: "Weapon",
       "button text": ["Buy Bronze Sword for 50","Buy Iron Sword for 100"," Buy Silver Sword for 150","Guild"],
-      "button func" :[,,,guildHeal],
+      "button func" :[bronzeSword,ironSword,silverSword,guildHeal],
       text: "Buying Weapon will help you to become the greatest Adventurer! "
     },
     {
